bbf02c74e0a7958fa00702cb1a3a1029c72d48c7
depascaldc <depascaldc@gmail.com>
2019-08-15 00:05:05 +0200

    fixed placeholder %newname

--- NicknamePlugin/src/main/java/de/depascaldc/nukkitxprojects/nick/commands/NickCommand.java
+++ NicknamePlugin/src/main/java/de/depascaldc/nukkitxprojects/nick/commands/NickCommand.java
@@ -19,45 +19,45 @@ public class NickCommand extends Command {
 		super(name);
 		setPermission("nickname.nick");
 		setDescription("Set your nickname (for OP's set your or other nicknames)");
-		setAliases(new String[] {"nick"});
+		setAliases(new String[] { "nick" });
 	}
 
 	@Override
 	public boolean execute(CommandSender sender, String label, String[] args) {
-		if(sender.hasPermission("nickname.nick")) {
+		if (sender.hasPermission("nickname.nick")) {
 			String usageop = "§cUse: §6/nickname (username / your nickname) (nickname)";
 			String usageplayer = "§cUse: §6/nickname (nickname)";
-			Player player = (Player)sender;
+			Player player = (Player) sender;
 			String success = "§aThe player §e%player's §aname is now §6%newname";
-			if(args.length > 1) {
+			if (args.length > 1) {
 				String target = args[0];
 				String nameToSetForTarget = args[1];
-				if(sender.isOp()) {
+				if (sender.isOp()) {
 					Player targetPlayer = player.getServer().getPlayer(target);
-					if(targetPlayer != null) {
+					if (targetPlayer != null) {
 						targetPlayer.setDisplayName(nameToSetForTarget);
-						sender.sendMessage(success.replaceAll("%player", targetPlayer.getName()));
-					} else 
+						sender.sendMessage(success.replaceAll("%player", targetPlayer.getName()).replaceAll("%newname",
+								nameToSetForTarget));
+					} else
 						sender.sendMessage("§cPlayer seems to be not online.");
 				} else {
 					sender.sendMessage(usageplayer);
 					return true;
 				}
-			} else if(args.length > 0) {
+			} else if (args.length > 0) {
 				String nameToSet = args[0];
 				player.setDisplayName(nameToSet);
-				sender.sendMessage(success.replaceAll("%player", player.getName()));
+				sender.sendMessage(success.replaceAll("%player", player.getName()).replaceAll("%newname",
+						nameToSet));
 			} else {
-				if(sender.isOp()) {
+				if (sender.isOp()) {
 					sender.sendMessage(usageop);
 				} else {
 					sender.sendMessage(usageplayer);
 				}
 			}
-		} 
+		}
 		return false;
 	}
-	
-	
 
 }
